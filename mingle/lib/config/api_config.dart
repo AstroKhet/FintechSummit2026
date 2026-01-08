b4d51c5aa473aa01822fa9244567b7545e2619f7
Lee Zhao Rui <zhaorui031202@gmail.com>
2026-01-08 17:50:35 +0800

    Update Render API Key

--- mingle/lib/config/api_config.dart
+++ mingle/lib/config/api_config.dart
@@ -2,7 +2,7 @@
 /// Store all API endpoints and base URLs here
 class ApiConfig {
   // Base URL for your API
-  static const String baseUrl = 'https://fintechsummit2026.onrender.com';
+  static const String baseUrl = 'https://fintechsummit2026-rqxl.onrender.com';
 
   // Auth endpoints
   static const String loginEndpoint = '/user/login';
