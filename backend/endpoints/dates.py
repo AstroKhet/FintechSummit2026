51bf60b4fb37ca9a4d7245f9f5352c642de930ae
Lee Zhao Rui <zhaorui031202@gmail.com>
2026-01-08 15:00:38 +0800

    Add create and getDate

--- backend/endpoints/dates.py
+++ backend/endpoints/dates.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request
 from database.database import find_one_collection, add_to_collection, update_to_collection, delete_from_collection
-from misc.misc import read_json, format_error_msg, format_success_msg
+from misc.misc import read_json, format_error_msg, format_success_msg, generate_unique_6_digit
 import hashlib
 import random
 import string
@@ -10,15 +10,84 @@ router = APIRouter()
 
 @router.post("/createDate")
 async def createDateRequest(request: Request):
-    dummy, error = await read_json(request, ["dummy"])
+    a_pubkey,
+    b_pubkey,
+    a_id,
+    b_id,
+    stake,
+    restaurant_pubkey,
+    deadline,
+    secA,
+    secB,
+    condA,
+    condB,
+    seqA,
+    seqB, 
+    error = await read_json(request, [
+        "a_pubkey",
+        "b_pubkey",
+        "a_id",
+        "b_id",
+        "stake",
+        "restaurant_pubkey",
+        "deadline",
+        "secA",
+        "secB",
+        "condA",
+        "condB",
+        "seqA",
+        "seqB"])
+
+    date_id = str(generate_unique_6_digit())
     if error:
         return format_error_msg(error)
-    return createDate(dummy)
+    return createDate(date_id,
+        a_pubkey,
+        b_pubkey,
+        a_id,
+        b_id,
+        stake,
+        restaurant_pubkey,
+        deadline,
+        secA,
+        secB,
+        condA,
+        condB,
+        seqA,
+        seqB)
 
-def createDate(dummy):
+def createDate(
+    date_id,
+    a_pubkey,
+    b_pubkey,
+    a_id,
+    b_id,
+    stake,
+    restaurant_pubkey,
+    deadline,
+    secA,
+    secB,
+    condA,
+    condB,
+    seqA,
+    seqB):
     date_jsn =  {
-                "dummy": dummy,
-                }
+        "date_id": date_id,
+        "a_pubkey": a_pubkey,
+        "b_pubkey": b_pubkey,
+        "a_id": a_id,
+        "b_id": b_id,
+        "stake": stake,
+        "restaurant_pubkey": restaurant_pubkey,
+        "deadline": deadline,
+        "secA": secA,
+        "secB": secB,
+        "condA": condA,
+        "condB": condB,
+        "seqA": seqA,
+        "seqB": seqB, 
+        "isCompleted": False 
+    }
     date = add_to_collection(date_jsn, "dates")
     return format_success_msg({"access": True})
 
@@ -36,5 +105,21 @@ def getDate(dateID):
     else:
         return format_error_msg("No date found with this ID")
 
-print(createDate("test"))
-getDate("test")
+# Testing
+
+temp = generate_unique_6_digit()
+print(createDate(temp,
+    "a_pubkey",
+    "b_pubkey",
+    "a_id",
+    "b_id",
+    100,
+    "restaurant_pubkey",
+    "deadline",
+    "secA",
+    "secB",
+    "condA",
+    "condB",
+    "seqA",
+    "seqB"))
+print(getDate(temp))
