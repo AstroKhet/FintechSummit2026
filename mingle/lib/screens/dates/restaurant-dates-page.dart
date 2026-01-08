b03948ea7007d5b4ad0de6613a247568ddc578c5
forestmu <e1354455@u.nus.edu>
2026-01-08 10:16:31 +0800

    Add dates page

--- mingle/lib/screens/dates/restaurant-dates-page.dart
+++ mingle/lib/screens/dates/restaurant-dates-page.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:mingle/screens/dates/restaurant-dates-card.dart';
 
-class RestaurantDatesPage extends StatelessWidget {
-  const RestaurantDatesPage({super.key});
+class RestaurantDatePage extends StatelessWidget {
+  RestaurantDatePage({super.key});
+
+  // To replace with getDatesArray or sth
+  // May need to get name & photo from user's ID separately
+  final List<Map<String, dynamic>> matches = [
+    {
+      "a": {
+        "id": "user_001",
+        "name": "Alex",
+        "photo": "https://randomuser.me/api/portraits/men/32.jpg",
+      },
+      "b": {
+        "id": "user_002",
+        "name": "Jamie",
+        "photo": "https://randomuser.me/api/portraits/women/44.jpg",
+      },
+      "dateTime": "2026-01-07T18:30:00",
+      "matchId": "match_001",
+    },
+     {
+      "a": {
+        "id": "user_003",
+        "name": "Charlie",
+        "photo": "https://randomuser.me/api/portraits/men/32.jpg",
+      },
+      "b": {
+        "id": "user_004",
+        "name": "Dawn",
+        "photo": "https://randomuser.me/api/portraits/women/44.jpg",
+      },
+      "dateTime": "2026-01-07T18:30:00",
+      "matchId": "match_001",
+    },
+     {
+      "a": {
+        "id": "user_005",
+        "name": "Ele",
+        "photo": "https://randomuser.me/api/portraits/men/32.jpg",
+      },
+      "b": {
+        "id": "user_006",
+        "name": "Faith",
+        "photo": "https://randomuser.me/api/portraits/women/44.jpg",
+      },
+      "dateTime": "2026-01-07T18:30:00",
+      "matchId": "match_001",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SizedBox.expand(
-          child: Text("Dates n the restaurant")
-        ),
-      ),
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: matches.length,
+      itemBuilder: (context, index) {
+        return RestaurantDateCard(match: matches[index]);
+      },
     );
   }
 }
